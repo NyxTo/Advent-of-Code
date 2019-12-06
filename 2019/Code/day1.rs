1b5c45e93b937cab7c6c1c66ce0ae7d513154f79
NyxTo <44107187+NyxTo@users.noreply.github.com>
2019-12-06 13:02:11 +0800

    Add files via upload

---
+++ 2019/Code/day1.rs
@@ -0,0 +1,17 @@
+use std::fs::read_to_string;
+
+fn main() {
+	let puzzle = read_to_string("in1.txt").unwrap();
+	let (mut sum_a, mut sum_b) = (0, 0);
+	for line in puzzle.lines() {
+		let mass = line.parse::<i32>().unwrap();
+		let mut fuel = mass / 3 - 2;
+		sum_a += fuel;
+		while fuel > 0 {
+			sum_b += fuel;
+			fuel = fuel / 3 - 2;
+		}
+	}
+	println!("Part A: {}", sum_a);
+	println!("Part B: {}", sum_b);
+}
---
+++ 2019/Code/day2.rs
@@ -0,0 +1,24 @@
+use std::fs::read_to_string;
+
+fn main() {
+	let input = read_to_string("in2.txt").unwrap().trim_end().to_string();
+	let ints = input.split(",").map(|int| int.parse::<u32>().unwrap()).collect::<Vec<_>>();
+	for noun in 0..100 {
+	for verb in 0..100 {
+		let mut code = ints.clone();
+		code[1] = noun; code[2] = verb;
+		for pos in (0 .. code.len()).step_by(4) {
+			let at = code[pos + 3];
+			let (val1, val2) = (code[code[pos + 1] as usize], code[code[pos + 2] as usize]);
+			code[at as usize] = match code[pos] {
+				99 => break,
+				1  => val1 + val2,
+				2  => val1 * val2,
+				_  => return panic!("fail"),
+			}
+		}
+		if noun == 12 && verb == 2 { println!("Part A: {}", code[0]); }
+		if code[0] == 19690720 { println!("Part B: {}", 100 * noun + verb); }
+	}
+	}
+}
