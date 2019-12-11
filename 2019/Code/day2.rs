4e26233f6b4a024544f223a157411c5efc250f0f
NyxTo <44107187+NyxTo@users.noreply.github.com>
2019-12-12 00:36:28 +0800

    Update day2.rs

--- 2019/Code/day2.rs
+++ 2019/Code/day2.rs
@@ -2,10 +2,10 @@ use std::fs::read_to_string;
 
 fn main() {
 	let input = read_to_string("in2.txt").unwrap().trim_end().to_string();
-	let ints = input.split(',').map(|int| int.parse::<u32>().unwrap()).collect::<Vec<_>>();
+	let code = input.split(',').map(|int| int.parse::<u32>().unwrap()).collect::<Vec<_>>();
 	for noun in 0..100 {
 	for verb in 0..100 {
-		let mut code = ints.clone();
+		let mut code = code.clone();
 		code[1] = noun; code[2] = verb;
 		for pos in (0 .. code.len()).step_by(4) {
 			let (val1, val2, at) = (code[code[pos + 1] as usize], code[code[pos + 2] as usize], code[pos + 3] as usize);
