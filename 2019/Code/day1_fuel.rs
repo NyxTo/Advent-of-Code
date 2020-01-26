0ffd33ddd7fe96f059472fe7abdccc4aaae900a7
NyxTo <44107187+NyxTo@users.noreply.github.com>
2020-01-26 12:18:13 +0800

    Add files via upload

--- 2019/Code/day1_fuel.rs
+++ 2019/Code/day1_fuel.rs
@@ -3,8 +3,7 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
 	let (mut sum_a, mut sum_b) = (0, 0);
-	for line in BufReader::new(File::open("in1.txt").unwrap()).lines() {
-		let mass = line.unwrap().parse::<i32>().unwrap();
+	for mass in BufReader::new(File::open("in1.txt").unwrap()).lines().map(|line| line.unwrap().parse::<i32>().unwrap()) {
 		let mut fuel = mass / 3 - 2;
 		sum_a += fuel;
 		while fuel > 0 {
@@ -12,6 +11,6 @@ fn main() {
 			fuel = fuel / 3 - 2;
 		}
 	}
-	println!("Part A: {}", sum_a); // 3331523
-	println!("Part B: {}", sum_b); // 4994396
+	println!("Part A: {}", sum_a);
+	println!("Part B: {}", sum_b);
 }
