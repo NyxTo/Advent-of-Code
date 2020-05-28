70c6a5246d2c456b87ed406ed5d9793bdbd90e45
NyxTo <44107187+NyxTo@users.noreply.github.com>
2020-05-28 19:05:38 +0800

    Update day_10_asteroid.rs

--- 2019/Code/day_10_asteroid.rs
+++ 2019/Code/day_10_asteroid.rs
@@ -12,25 +12,24 @@ fn gcd(mut a: usize, mut b: usize) -> usize {
 }
 
 fn main() {
-	let grid = BufReader::new(File::open("in10.txt").unwrap()).lines().map(|row| row.unwrap().chars().collect::<Vec<_>>()).collect::<Vec<_>>();
+	let grid = BufReader::new(File::open("in_10.txt").unwrap()).lines().map(|row| row.unwrap().chars().collect::<Vec<_>>()).collect::<Vec<_>>();
 	let (hgt, wid, mut max_det, mut vapour) = (grid.len(), grid[0].len(), 0, (0, 0));
 	for sy in 0..hgt {
 	for sx in 0..wid {
 	if grid[sy][sx] == '#' {
-		let (mut detect, mut ang, mut btwn, mut aster) = (0, vec![vec![0.; wid]; hgt], vec![vec![0; wid]; hgt], vec![]);
+		let (mut detect, mut btwn, mut aster, mut ang) = (0, vec![vec![0; wid]; hgt], vec![], vec![vec![0.; wid]; hgt]);
 		for ey in 0..hgt {
 		for ex in 0..wid {
 		if grid[ey][ex] == '#' {
 			if ex == sx && ey == sy { continue; }
-			ang[ey][ex] = (ex as f64 - sx as f64).atan2(ey as f64 - sy as f64);
 			let num = gcd(max(sx, ex) - min(sx, ex), max(sy, ey) - min(sy, ey));
 			btwn[ey][ex] = (1..num).filter(|i| {
 				let (bx, by) = ((sx * (num - i) + ex * i) / num, (sy * (num - i) + ey * i) / num);
 				grid[by][bx] == '#'
 			}).count();
 			if btwn[ey][ex] == 0 { detect += 1; }
-			else { continue; }
 			aster.push((ex, ey));
+			ang[ey][ex] = (ex as f64 - sx as f64).atan2(ey as f64 - sy as f64);
 		}
 		}
 		}
@@ -38,7 +37,7 @@ fn main() {
 			max_det = detect;
 			aster.sort_by(|(x1, y1), (x2, y2)| ang[*y2][*x2].partial_cmp(&ang[*y1][*x1]).unwrap());
 			aster.sort_by_key(|(x, y)| btwn[*y][*x]);
-			if aster.len() >= 200 { vapour = aster[199]; }
+			vapour = aster[199];
 		}
 	}
 	}
